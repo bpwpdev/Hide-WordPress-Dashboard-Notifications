0c75b197d3077ac2f0f84c0bd5ef85465ac3c338
Buddhika <dev@buddhikaperera.com>
2018-04-16 10:45:36 +0530

    Update to code base
    Minor changes to the functions of the plugin and an update to the readme.md file.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# WordPress-Dashboard-Notifications
-A plugin to hide WordPress Theme, Plugin and Core Update Notifications in Dashboard 
+=== Hide WordPress Dashboard Notifications ===
+Contributors: @buddhikawp
+Tags: wordpress, dashboard, notifications
+Requires at least: 4.1
+Tested up to: 7.1
+Requires PHP: 5.2
+Stable tag: 7.1
+License: GPL-3.0+
+License URI: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+
+A WordPress plugin to disable Dashboard notifications.
+
+== Description ==
+A WordPress plugin to disable annoying WordPress Core, Theme and Plugin updates in Dashboard. Installation of this plugin as at your on risk. WordPress recommends that all core files, themes and plugins need to be kept updated for security and performance. If you use this plugin, make sure you keep the website up to date with new releases of your active WordPress version, plugins
+and themes and update them as new versions are released (simply by deactivating this plugin).
+
+[WordPress Website Developer](https://www.buddhikaperera.com) | [Follow me on Twitter](http://twitter.com/buddhikaperera_)
+
+== Installation ==
+
+1. Download the plugin and unzip it.
+2. Upload the folder wordpress-dashboard-notifications/ to your /wp-content/plugins/ folder.
+3. Activate the plugin from your WordPress admin panel.
+4. Installation finished.
+
+== Changelog ==
+1.0
--- wpdashnotification.php
+++ wpdashnotification.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Plugin Name: WordPress Hide Dashboard Notifictions
- * Plugin URI:  https://github.com/buddhikaperera/WordPress-Dashboard-Notifications
+ * Plugin Name: Hide WordPress Dashboard Notifications
+ * Plugin URI:  https://github.com/buddhikaperera/Hide-WordPress-Dashboard-Notifications
  * Description: A plugin to remove WordPress Core, Theme and Plugin Notifictions in WordPress Dashboard
  * Author:      Buddhika Perera
  * Author URI:  https://www.buddhikaperera.com
  * Version:     1.0
- * Text Domain: wp-dash-ntfs
+ * Text Domain: hwdn
  * Domain Path: languages
  * License:     GPL-3.0+
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
@@ -18,10 +18,14 @@
  }
 
 // hide update notifications
-function remove_core_updates(){
-global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+function remove_core_updates () {
+     global $wp_version;
+     return(object) array(
+          'last_checked'=> time(),
+          'version_checked'=> $wp_version,
+          'updates' => array()
+     );
 }
-
-add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
-add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
-add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
